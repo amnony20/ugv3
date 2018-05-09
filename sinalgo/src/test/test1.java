e2e81f4fdef040c1165aad8a2fcf777e31b145f5
amity <amity@LAPTOP-FMANBRAL>
2018-05-10 01:46:31 +0300

    fvfvf

---
+++ sinalgo/src/test/test1.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.Test;
+import sinalgo.nodes.Node;
+
+import junit.framework.TestCase;
+import projects.ugv.nodes.nodeImplementations.UGVNode;
+
+public class test1 extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	 @Test
+	    public void testexample() {
+	       
+	        assertEquals(1,1);
+	    }
+	 @Test
+	    public void testUGVnode() {
+	       
+	       // Node node = new Node();
+	        assertEquals(1,1);
+	    }
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
